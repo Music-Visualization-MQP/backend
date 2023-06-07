c8a10d27ebc8402578b4bda18349a34f900d5f2d
ibixler <108148378+ibixler@users.noreply.github.com>
2023-06-07 11:23:34 -0400

    main isnt broken anymore added libs

--- app/src/main/java/collageify/App.java
+++ app/src/main/java/collageify/App.java
@@ -2,7 +2,7 @@ package collageify;
 
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
-
+import org.apache.cxf.transport.http.*;
 import collageify.db.SQLTime;
 import collageify.musicService.Playing;
 import collageify.user.User;
@@ -19,15 +19,23 @@ public class App {
         Playing song = new Playing(0, "ibutts", 30000, "https://open.spotify.com/track/6lrDckuosGpwEHtm1hHBcf?si=c8286ba2785f4196", "Gorillaz", "Demon Days", "November Has Come",55, 165000);
         song.UpdateProgress(165000);
         song.UpdateDB();
-        User user = new User("ldoggs","lawhitley@gmail.com", "somethingEmbarassing", "Laurel", "Whitley");
+        /* User user = new User("ldoggs","lawhitley@gmail.com", "somethingEmbarassing", "Laurel", "Whitley");
         user.UpdateDB();
-        
-        /* JAXRSServerFactoryBean factoryBean = new JAXRSServerFactoryBean();   
-        factoryBean.setResourceClasses(collageify.web.TestClass.class);
-        factoryBean.setResourceProvider(collageify.web.TestClass.class, new SingletonResourceProvider(new TestClass()));
-        factoryBean.setAddress("http://localhost:80/");
-        factoryBean.create();   
-        System.out.println("Server started at http://localhost:80/"); */
+         */
+        JAXRSServerFactoryBean factoryBean = new JAXRSServerFactoryBean();   
+        try{
+            factoryBean.setResourceClasses(collageify.web.TestClass.class);
+            
+            factoryBean.setResourceProvider(collageify.web.TestClass.class, new SingletonResourceProvider(new TestClass()));
+            
+            factoryBean.setAddress("http://localhost:80/");
+            
+            factoryBean.create();   
+            System.out.println("Server started at http://localhost:80/"); 
+
+        } catch (Exception e){
+            throw e;
+        }
     }
 
 }
