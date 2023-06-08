c2303195317900ec71808dec728c268958e82809
ibixler <108148378+ibixler@users.noreply.github.com>
2023-06-08 11:49:34 -0400

    now when you press login you get 8... yay

--- app/src/main/java/collageify/api/TestClass.java
+++ app/src/main/java/collageify/api/TestClass.java
@@ -1,18 +1,33 @@
 package collageify.api;
 
 
+import java.io.IOException;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
-
+import jakarta.ws.rs.ext.Provider;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 
 @Path("/api")
 public class TestClass {
+
+
+
     @GET
+    @Path("/test")
     @Produces(MediaType.TEXT_PLAIN)
-    public String sayHello(){
-        return "hey there sexy";
+    public int getRandomInteger() {
+        // Generate a random integer
+        int randomInteger = 8;
+
+        return randomInteger;
     }
 }
