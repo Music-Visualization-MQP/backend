903b8ec7bce873e2b8f7afad7969cf1e758c4f7b
ibixler <108148378+ibixler@users.noreply.github.com>
2023-06-10 10:15:36 -0400

    coppied some example from github on how to use the sp api

---
+++ app/src/main/java/collageify/api/loginServelet.java
@@ -0,0 +1,11 @@
+package collageify.api;
+//import jakarta.servlet.ServeletException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+
+
+public class loginServelet {
+
+}
